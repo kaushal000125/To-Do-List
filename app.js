1ef8e338ab192d9a0037b221917026c7718b2310
Kaushal Chakardhari <kaushal000126@gmail.com>
2021-06-15 09:33:28 +0530

    add port

--- app.js
+++ app.js
@@ -160,10 +160,10 @@ app.get("/about", function(req, res){
   res.render("about");
 });
 let port =process.env.PORT;
-/*if(port==null||port=="")
+if(port==null||port=="")
 {
   port=3000;
-}*/
-app.listen(port||3000, function() {
+}
+app.listen(port, function() {
   console.log("Server started successfully");
 });
